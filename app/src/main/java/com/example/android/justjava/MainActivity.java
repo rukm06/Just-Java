fb1a5c09ee7f6041f82276604b0fbe8563568f81
Dean <dean.qiu73@gmail.com>
2017-05-14 23:41:45 +0800

    Add below functions: 1. EditText for input name 2. CheckBox for topping 3. Scrolling bar 4. Forming an e-mail of summary after clicking "ORDER" 5. Boundary check for number of quantities 6. Support Spanish and Tranditional Chinese

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,10 +6,15 @@
  */
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -30,16 +35,79 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        //displayPrice(quantity * 5);
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        displayMessage(priceMessage + "\nThank you!");
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        //Log.v("MainActivity", "hasWhippedCream state is: " + hasWhippedCream);
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        //Log.v("MainActivity", "Chocolate: " + hasChocolate);
+
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        //Log.v("MainActivity", "Name: " + name);
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        String orderSummary = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+//        displayMessage(orderSummary);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject, name));
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate    is whether or not the user wants chocolate topping
+     * @return total price
+     */
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Base price of 1 cup of coffee
+        int pricePerCup = 5;
+        // Add $1 if the user wants whipped cream
+        if (addWhippedCream) pricePerCup += 1;
+        // Add $2 if the user wants chocolate
+        if (addChocolate) pricePerCup += 2;
+        // Calculate the total order price by multiplying by quantity
+        return quantity * pricePerCup;
+    }
+
+    /**
+     * Create the summary of the order.
+     *
+     * @param price           of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate    is whether or not the user wants chocolate topping
+     * @param name            of the customer
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String name) {
+        String message = getString(R.string.order_summary_name, name);
+        message += "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream);
+        message += "\n" + getString(R.string.order_summary_chocolate, addChocolate);
+        message += "\n" + getString(R.string.order_summary_quantity, quantity);
+        message += "\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        message += "\n" + getString(R.string.thank_you);
+        return message;
     }
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You can not have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
     }
@@ -48,6 +116,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You can not have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
     }
@@ -60,20 +134,12 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
+//    /**
+//     * This method displays the given text on the screen.
+//     */
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.orderSummary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
 
 }
